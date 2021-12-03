aff6ec3433b674e5a024badddbe4a06476575533
kaitlyn-liao <59896595+kaitlyn-liao@users.noreply.github.com>
2021-12-03 11:08:31 -0800

    random pet wroking and dispalys

--- js/pet.js
+++ js/pet.js
@@ -26,7 +26,7 @@ var petImage = document.getElementsByClassName('profile-img');
 let g_adoptedPets = [];
 
 // Not sure how to do this yet but we will set it to a Pet object
-let g_currentPet;
+let g_currentPet = {};
 
 // Global Variables for Settings
 let g_colorPref = "";
@@ -34,23 +34,22 @@ let g_breedPref = "";
 
 
 // CONSOLE LOG FOR TESTING OUR FUNCTIONS
-console.log(getRandomName());
+// console.log(getRandomName());
 
 //function to handle random naming conventions
 //adj + noun
-function getRandomName(){
+function getRandomName() {
     //where is the pets name going to show up on the index.html?
     //this is where we figure that out.
 
     //get a random adjective from our adjective list
-    var randomAdj = nameAdjectives[Math.floor(Math.random()*nameAdjectives.length)];
+    var randomAdj = nameAdjectives[Math.floor(Math.random() * nameAdjectives.length)];
     //get a random adjective from our adjective list
-    var randomNoun = nameNouns[Math.floor(Math.random()*nameNouns.length)];
+    var randomNoun = nameNouns[Math.floor(Math.random() * nameNouns.length)];
 
     //assign the pets brand-spanking-new name!
     var newPetName = randomAdj + " " + randomNoun;
     //return our pets cool name!
-    document.getElementById("petName").innerHTML = newPetName;
     return newPetName;
 }
 
@@ -67,8 +66,30 @@ function setFilters(color_pref, breed_pref) {
 // FUNCTION USING API
 // This function will make a GET call to access our API and
 // return a random fact or random joke.
-function getRandomFact(){
+function getRandomFact() {
 
+}
+
+// this function will pull from our json file to get a random pet
+function getRandomPet() {
+    var pet = {};
+    var pet_name = getRandomName();
+    fetch("./pets.json",)
+        .then(response => {
+            // console.log(response.json());
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            // gets random pet object 
+            var random_idx = Math.floor( Math.random() * 9 );
+            pet = data.pets[random_idx];
+            // set name to name field of pet object
+            pet.name = pet_name;
+            g_currentPet = pet;
+            console.log(pet);
+            displayPet();
+        });
 }
 
 
@@ -81,18 +102,18 @@ function getRandomFact(){
 // name, extras, and the random fact/joke.
 function displayPet() {
     let fact = getRandomFact();
-    g_currentPet = getRandomPet();
-    let name = getRandomName();
-    // set name to name field of g_currentPet pet object
 
-    // DISPLAY ON WEBPAGE
+    // displays name on webpage
+    document.getElementById("petName").innerHTML = g_currentPet.name;
+    // displays img on webpage
+    document.getElementById("pet-image").src = g_currentPet.img;
 }
 
 // This function will be called every time the user clicks
 // the Adopt Me button on the UI.
 function adoptPet() {
     // First it will check if the array is full (max length will be 6).
-    if(g_adoptedPets.length == 6){
+    if (g_adoptedPets.length == 6) {
         // If it is ‘full’ we will remove the first pet in the array
     }
     g_adoptedPets.append(g_currentPet);
@@ -104,11 +125,11 @@ function adoptPet() {
 }
 
 function getRandomColor() {
-  var colorLetters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += colorLetters[Math.floor(Math.random() * 16)];
-  }
+    var colorLetters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += colorLetters[Math.floor(Math.random() * 16)];
+    }
 
-  return color;
+    return color;
 }
